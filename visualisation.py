3ea3bf825513ea93002dccea90688419ff1c5ef8
Lucy Ivatt <luceapuce@gmail.com>
2022-03-08 19:18:28 +0000

    added some doorway coordinates and created visualisation function to plot on minimap

--- visualisation.py
+++ visualisation.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from awpy.visualization.plot import plot_map, position_transform
 from awpy.data import MAP_DATA
 from configparser import ConfigParser
+import json
 
 SMOKE_DIAMETER_UNITS = 288
 
@@ -28,17 +29,34 @@ def plot_all_smokes(rounds, map_name, map_type="simpleradar", dark=True):
 
 
 def draw_introduction_figures():
-    overpass_game = DemoParser.read_json(
-        demo_dir + "misc\\introduction_demos\\")
+    pass
+    # overpass_game = DemoParser.read_json(
+    #     demo_dir + "misc\\introduction_demos\\")
 
 
 def draw_doorways():
-    pass
+    entrances_file = open("de_mirage_entrances.json")
+    entrances_data = json.load(entrances_file)
+    fig, a = plot_map(map_name="de_mirage", map_type="simpleradar")
+    fig.set_size_inches(18.5, 10.5)
+
+    for entrance in entrances_data.values():
+        x1 = position_transform("de_mirage", entrance["x1"], "x")
+        x2 = position_transform("de_mirage", entrance["x2"], "x")
+        y1 = position_transform("de_mirage", entrance["y1"], "y")
+        y2 = position_transform("de_mirage", entrance["y2"], "y")
+        a.plot([x1, x2], [y1, y2], color="#FF10F0", linewidth=2.5)
+        a.scatter(x1, y1, color="#FF10F0")
+        a.scatter(x2, y2, color="#FF10F0")
+    fig.savefig(config["Outputs"]["image_save_location"] +
+                '\\doorway_locations.png', dpi=100)
+    plt.show()
 
 
-location = demo_dir + "\\misc\\introduction_demos\\"
-demo_parser_1 = DemoParser(
-    demofile=location + "natus-vincere-vs-g2-m1-inferno.dem", outpath=location)
-demo_parser_2 = DemoParser(
-    demofile=location + "natus-vincere-vs-g2-m2-mirage.dem", outpath=location)
-demo1, demo2 = demo_parser_1.parse_demo(), demo_parser_2.parse_demo()
+# location = demo_dir + "\\misc\\introduction_demos\\"
+# demo_parser_1 = DemoParser(
+#     demofile=location + "natus-vincere-vs-g2-m1-inferno.dem", outpath=location)
+# demo_parser_2 = DemoParser(
+#     demofile=location + "natus-vincere-vs-g2-m2-mirage.dem", outpath=location)
+# demo1, demo2 = demo_parser_1.parse_demo(), demo_parser_2.parse_demo()
+draw_doorways()
